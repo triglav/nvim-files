83cede01b69bf06fc7f2e59d49319a9c6f92cdb1
Matej Svec <matej@msvec.sk>
2023-04-23 21:45:38 +0200

    Disable bufferline

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,35 @@
+return {
+  -- Disable bufferline
+  { "akinsho/bufferline.nvim", enabled = false },
+  -- Show lualine for all buffers, display full path in inactive buffers
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function()
+      local icons = require("lazyvim.config").icons
+      return {
+        options = {
+          globalstatus = false,
+        },
+        sections = {
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+        inactive_sections = {
+          lualine_c = {
+            {
+              "diagnostics",
+              symbols = {
+                error = icons.diagnostics.Error,
+                warn = icons.diagnostics.Warn,
+                info = icons.diagnostics.Info,
+                hint = icons.diagnostics.Hint,
+              },
+            },
+            { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+            { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+          },
+        },
+      }
+    end,
+  },
+}
