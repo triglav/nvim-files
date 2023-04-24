d1087b8d50b89f1219ad3e65b8b5867995f5062f
Matej Svec <matej@msvec.sk>
2023-04-24 17:08:04 +0200

    Simplify telescope mappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,5 @@
+local Util = require("lazyvim.util")
+
 return {
   {
     "telescope.nvim",
@@ -7,6 +9,17 @@ return {
       config = function()
         require("telescope").load_extension("fzf")
       end,
+    },
+    keys = {
+      { "<leader>/", Util.telescope("live_grep", { cwd = false }), desc = "Grep" },
+      { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+      { "<leader><space>", Util.telescope("files", { cwd = false }), desc = "Find Files" },
+      { "<leader>ff", Util.telescope("files", { cwd = false }), desc = "Find Files" },
+      { "<leader>fF", false },
+      { "<leader>sg", Util.telescope("live_grep", { cwd = false }), desc = "Grep" },
+      { "<leader>sG", false },
+      { "<leader>sw", Util.telescope("grep_string", { cwd = false }), desc = "Word" },
+      { "<leader>sW", false },
     },
   },
 }
