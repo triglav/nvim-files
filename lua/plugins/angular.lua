c005d41cbcc344b11831a5075280a2bea0d2fc05
Matej Svec <matej.svec@iblsoft.com>
2023-07-06 17:29:00 +0200

    Add neotest with jest support

--- lua/plugins/angular.lua
+++ lua/plugins/angular.lua
@@ -21,4 +21,14 @@ return {
       vim.list_extend(opts.ensure_installed, { "css-lsp", "html-lsp", "stylelint-lsp" })
     end,
   },
+  {
+    "nvim-neotest/neotest",
+    -- dependencies = { "haydenmeade/neotest-jest" },
+    -- The following fixes tests not working in NX monorepo
+    -- Can be reverted once https://github.com/nvim-neotest/neotest-jest/pull/64 is merged
+    dependencies = { "guivazcabral/neotest-jest" },
+    opts = function(_, opts)
+      table.insert(opts.adapters, require("neotest-jest")({}))
+    end,
+  },
 }
