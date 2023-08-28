b5a9f332020d3b09e66f037aba96364d1a6aeabd
Matej Svec <matej.svec@iblsoft.com>
2023-08-28 11:44:24 +0200

    Keep the fugitive windows size stable

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,10 +2,23 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = { "*.frag", "*.vert" },
   callback = function()
     vim.opt.filetype = "glsl"
     vim.opt.commentstring = "// %s"
+  end,
+})
+
+-- make fugitive windows size stable
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("fugitive_au"),
+  pattern = { "fugitive" },
+  callback = function()
+    vim.opt_local.winfixheight = true
   end,
 })
