9a689d34589faa3b3997315af43a34f446f46649
Matej Svec <matej.svec@iblsoft.com>
2024-03-08 09:11:15 +0100

    Enforce prettierd formatter

--- lua/plugins/angular.lua
+++ lua/plugins/angular.lua
@@ -32,4 +32,29 @@ return {
       table.insert(opts.adapters, require("neotest-jest")({}))
     end,
   },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["javascript"] = { "prettierd" },
+        ["javascriptreact"] = { "prettierd" },
+        ["typescript"] = { "prettierd" },
+        ["typescriptreact"] = { "prettierd" },
+        ["vue"] = { "prettierd" },
+        ["css"] = { "prettierd" },
+        ["scss"] = { "prettierd" },
+        ["less"] = { "prettierd" },
+        ["html"] = { "prettierd" },
+        ["json"] = { "prettierd" },
+        ["jsonc"] = { "prettierd" },
+        ["yaml"] = { "prettierd" },
+        ["markdown"] = { "prettierd" },
+        ["markdown.mdx"] = { "prettierd" },
+        ["graphql"] = { "prettierd" },
+        ["handlebars"] = { "prettierd" },
+        ["angular"] = { "prettierd" },
+      },
+    },
+  },
 }
