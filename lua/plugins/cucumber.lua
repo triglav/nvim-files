8c5c940752df2509b6c6bf3be09eb6b53d44bd98
Matej Svec <matej@msvec.sk>
2024-03-15 21:29:51 +0100

    Add cucumber <-> rust LSP <<< DISABLED - DOES NOT WORK

---
+++ lua/plugins/cucumber.lua
@@ -0,0 +1,20 @@
+return {
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   ---@class PluginLspOpts
+  --   opts = {
+  --     ---@type lspconfig.options
+  --     servers = {
+  --       cucumber_language_server = {
+  --         -- cmd = { "/home/msvec/code/cucumber-language-server/bin/cucumber-language-server.cjs", "--stdio" },
+  --         settings = {
+  --           cucumber = {
+  --             features = { "tests/features/*.feature" },
+  --             glue = { "tests/steps/*.rs" },
+  --           },
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
+}
