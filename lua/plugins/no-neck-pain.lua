541dc0f31424e8d73359d31e1ce216bf8adf44fe
Matej Svec <matej@msvec.sk>
2025-05-03 21:20:46 +0200

    Add no-neck-pain plugin

---
+++ lua/plugins/no-neck-pain.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "shortcuts/no-neck-pain.nvim",
+    cmd = NoNeckPain,
+    keys = {
+      { "<leader>uN", "<CMD>NoNeckPain<CR>", desc = "No Neck Pain", remap = true },
+    },
+    opts = {
+      width = 120,
+    },
+  },
+}
