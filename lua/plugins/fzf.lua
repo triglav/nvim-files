eafd68f3cdd923d35ad756350047e5f6916d9130
Matej Svec <matej@msvec.sk>
2025-06-17 00:14:56 +0200

    Switch to fzf-lua from telescope

---
+++ lua/plugins/fzf.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    keys = {
+      { "<leader>/", LazyVim.pick("live_grep", { root = false }), desc = "Grep" },
+      { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files" },
+      { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files" },
+      { "<leader>fF", false },
+      { "<leader>sg", LazyVim.pick("live_grep", { root = false }), desc = "Grep" },
+      { "<leader>sG", false },
+      { "<leader>sw", LazyVim.pick("grep_cword", { root = false }), desc = "Word Match" },
+      { "<leader>sW", false },
+      { "<leader>sw", LazyVim.pick("grep_visual", { root = false }), mode = "v", desc = "Selection" },
+      { "<leader>sW", false, mode = "v" },
+    },
+  },
+}
