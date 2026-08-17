46f3bb02c8f67aa727d446b9d2e62bd64133223c
Matej Svec <matej.svec@iblsoft.com>
2026-08-17 08:57:30 +0200

    Clean-up Angular config
    keep just disable inlay hints

--- lua/plugins/angular.lua
+++ lua/plugins/angular.lua
@@ -1,24 +1,9 @@
-local util = require("lspconfig.util")
-
 return {
   {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     opts = {
       inlay_hints = { enabled = false },
-      servers = {
-        angularls = {
-          root_dir = util.root_pattern("angular.json", "project.json"),
-        },
-      },
-      setup = {
-        angularls = function()
-          Snacks.util.lsp.on({ name = "angularls" }, function(_, client)
-            --HACK: disable angular renaming capability due to duplicate rename popping up
-            client.server_capabilities.renameProvider = false
-          end)
-        end,
-      },
     },
   },
   {
